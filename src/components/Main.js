9f3fc754c8e87ee417f1bb92b58cccd458f99645
sumoli <1390031812@qq.com>
2016-06-30 16:36:07 +0800

    去掉测试

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
-//测试
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 //let yeomanImage = require('../images/yeoman.png');
